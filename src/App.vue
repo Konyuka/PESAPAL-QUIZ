ae27a2bc3d30b8117ff5fcccabca7ce46124987c
Michael Saiba <michaelsaiba84@gmail.com>
2023-01-26 15:04:57 +0300

    first question done.To work on documentation later

--- src/App.vue
+++ src/App.vue
@@ -25,15 +25,123 @@ const assembler = (textProgram) => {
       machineCodes.push(0x00);
     }
     else if (operationCode === "li"){
-      // since we have two items in the instructionItems array we identify by index
+      // since we have two items in the instructionItems array we identify what we need by index
       let register = instructionItems[1];
       let value = instructionItems[2];
-      let code = (0x02 << 12) | (register << 8) | (value & 0xFF);
-      machineCodes.push(code);
+      // shift 0x02 left by 12bits, register left by 8bits then keep the value in the range of 8 bit
+      let uniqueValue = (0x02 << 12) | (register << 8) | (value & 0xFF);
+      machineCodes.push(uniqueValue);
     }
+    else if (operationCode === "lw"){
+      // since we have two items in the instructionItems array we identify what we need by index
+      let register1 = instructionItems[1];
+      let register2 = instructionItems[2];
 
-  
-    
+      // shift 0x02 left by 12bits, register left by 8bits then combine the result of value AND 0xFF
+      let uniqueValue = (0x03 << 12) | (register1 << 8) | (register2 << 4);
+      machineCodes.push(uniqueValue);
+    }
+    else if (operationCode === "sw"){
+      // since we have two items in the instructionItems array we identify what we need by index
+      let register1 = instructionItems[1];
+      let register2 = instructionItems[2];
+
+      // shift 0x02 left by 12bits, register left by 8bits then combine the result of value AND 0xFF
+      let uniqueValue = (0x04 << 12) | (register1 << 8) | (register2 << 4);
+      machineCodes.push(uniqueValue);
+    }
+    else if (operationCode === "add"){
+      // since we have two items in the instructionItems array we identify what we need by index
+      let register1 = instructionItems[1];
+      let register2 = instructionItems[2];
+      let register3 = instructionItems[3];
+
+      // shift 0x02 left by 12bits, register left by 8bits then combine the result of value AND 0xFF
+      let uniqueValue = (0x05 << 12) | (register1 << 8) | (register2 << 4 | register3);
+      machineCodes.push(uniqueValue);
+    }
+    else if (operationCode === "sub"){
+      // since we have two items in the instructionItems array we identify what we need by index
+      let register1 = instructionItems[1];
+      let register2 = instructionItems[2];
+      let register3 = instructionItems[3];
+
+      // shift 0x02 left by 12bits, register left by 8bits then combine the result of value AND 0xFF
+      let uniqueValue = (0x06 << 12) | (register1 << 8) | (register2 << 4 | register3);
+      machineCodes.push(uniqueValue);
+    }
+    else if (operationCode === "mult"){
+      // since we have two items in the instructionItems array we identify what we need by index
+      let register1 = instructionItems[1];
+      let register2 = instructionItems[2];
+      let register3 = instructionItems[3];
+
+      // shift 0x02 left by 12bits, register left by 8bits then combine the result of value AND 0xFF
+      let uniqueValue = (0x07 << 12) | (register1 << 8) | (register2 << 4 | register3);
+      machineCodes.push(uniqueValue);
+    }
+    else if (operationCode === "div"){
+      // since we have two items in the instructionItems array we identify what we need by index
+      let register1 = instructionItems[1];
+      let register2 = instructionItems[2];
+      let register3 = instructionItems[3];
+
+      // shift 0x02 left by 12bits, register left by 8bits then combine the result of value AND 0xFF
+      let uniqueValue = (0x08 << 12) | (register1 << 8) | (register2 << 4 | register3);
+      machineCodes.push(uniqueValue);
+    }
+    else if (operationCode === "j"){
+      // since we have two items in the instructionItems array we identify what we need by index
+      let memoryLocation = instructionItems[1];
+
+      // shift 0x02 left by 12bits, register left by 8bits then combine the result of value AND 0xFF
+      let uniqueValue = (0x09 << 12) | (memoryLocation & 0xFFF);
+      machineCodes.push(uniqueValue);
+    }
+    else if (operationCode === "jr"){
+      // since we have two items in the instructionItems array we identify what we need by index
+      let register1 = instructionItems[1];
+
+      // shift 0x02 left by 12bits, register left by 8bits then combine the result of value AND 0xFF
+      let uniqueValue = (0x0A << 12) | (register1 & 0xFFF);
+      machineCodes.push(uniqueValue);
+    }
+    else if (operationCode === "beq"){
+      // since we have two items in the instructionItems array we identify what we need by index
+      let register1 = instructionItems[1];
+      let register2 = instructionItems[2];
+      let register3 = instructionItems[3];
+
+      // shift 0x02 left by 12bits, register left by 8bits then combine the result of value AND 0xFF
+      let uniqueValue = (0x0B << 12) | (register1 << 8) | (register2 << 4) | register3;
+      machineCodes.push(uniqueValue);
+    }
+    else if (operationCode === "bne"){
+      // since we have two items in the instructionItems array we identify what we need by index
+      let register1 = instructionItems[0];
+      let register2 = instructionItems[1];
+      let register3 = instructionItems[2];
+
+      // shift 0x02 left by 12bits, register left by 8bits then combine the result of value AND 0xFF
+      let uniqueValue = (0x0C << 12) | (register1 << 8) | (register2 << 4) | register3;
+      machineCodes.push(uniqueValue);
+    }
+    else if (operationCode === "inc"){
+      // since we have two items in the instructionItems array we identify what we need by index
+      let register1 = instructionItems[1];
+
+      // shift 0x02 left by 12bits, register left by 8bits then combine the result of value AND 0xFF
+      let uniqueValue = (0x0A << 12) | (register1 & 0xFFF);
+      machineCodes.push(uniqueValue);
+    }
+    else if (operationCode === "dec"){
+      // since we have two items in the instructionItems array we identify what we need by index
+      let register1 = instructionItems[1];
+
+      // shift 0x02 left by 12bits, register left by 8bits then combine the result of value AND 0xFF
+      let uniqueValue = (0x0A << 12) | (register1 & 0xFFF);
+      machineCodes.push(uniqueValue);
+    }
   }
 
 
